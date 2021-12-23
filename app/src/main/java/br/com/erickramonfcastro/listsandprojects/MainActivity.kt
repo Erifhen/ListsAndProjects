6ad447a4daffdbd9e64642363eb26e4124708ee2
Erick Ramon <erickramonfcastro@gmail.com>
2021-12-23 00:51:55 -0300

    base do projeto.2

--- app/src/main/java/br/com/erickramonfcastro/listsandprojects/MainActivity.kt
+++ app/src/main/java/br/com/erickramonfcastro/listsandprojects/MainActivity.kt
@@ -5,18 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import br.com.erickramonfcastro.listsandprojects.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
         //botões que ainda não fazem nada
-      val sharedList = findViewById<Button>(R.id.btSharedList)
-        sharedList.setOnClickListener {
+        binding.btSharedList.setOnClickListener {
             Toast.makeText(applicationContext, "Coming soon", Toast.LENGTH_LONG).show()
         }
-        findViewById<Button>(R.id.btAddFolder).setOnClickListener {
+        binding.btAddFolder.setOnClickListener {
             Toast.makeText(applicationContext, "Coming soon", Toast.LENGTH_LONG).show()
             //botões que mudam de Activity
             findViewById<Button>(R.id.btMenuProjects).setOnClickListener {
