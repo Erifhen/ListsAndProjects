e3e71bf7e5cde4c08419090b55da5fd63453c1cc
Erick Ramon <erickramonfcastro@gmail.com>
2021-12-27 17:42:17 -0300

    arrumado mahomenos

---
+++ app/src/main/java/br/com/erickramonfcastro/listsandprojects/AddProjectActivity.kt
@@ -0,0 +1,11 @@
+package br.com.erickramonfcastro.listsandprojects
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddProjectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_project)
+    }
+}
--- app/src/main/java/br/com/erickramonfcastro/listsandprojects/Projects.kt
+++ app/src/main/java/br/com/erickramonfcastro/listsandprojects/Projects.kt
@@ -1,11 +1,23 @@
 package br.com.erickramonfcastro.listsandprojects
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.erickramonfcastro.listsandprojects.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class Projects : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_projects)
+
+        findViewById<FloatingActionButton>(R.id.btAddProject).setOnClickListener {
+            val intent = Intent(this, AddProjectActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
